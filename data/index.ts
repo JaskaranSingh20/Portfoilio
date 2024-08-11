e8c3db4fc3096501caca9817cceb0433c5a37945
Jaskaran <hanjrajaskaran12@gmail.com>
2024-08-11 17:01:49 +0530

    Complete Portfolio

---
+++ data/index.ts
@@ -0,0 +1,201 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "./b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "./grid.svg",
+    spareImg: "./b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "./b5.svg",
+    spareImg: "./grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "3D Shirt Customization Tool",
+    des: "It previews the 3D model of shirt which we can customize using AI model.",
+    img: "./3dShirt.png",
+    iconLists: [
+      "./re.svg",
+      "./tail.svg",
+      "./three.svg",
+      "./fm.svg",
+      "./node.png",
+    ],
+    link: "https://3d-customizer-tool.netlify.app",
+  },
+  {
+    id: 2,
+    title: "Duo Studio Website",
+    des: "A creative website made with modern animations and modern UI/UX design.",
+    img: "./duo.png",
+    iconLists: ["./html.png", "./css.png", "./js.png", "./gsap.svg"],
+    link: "https://duostudioo.netlify.app",
+  },
+  {
+    id: 3,
+    title: "Disney+ Clone",
+    des: "This is Disney plus clone web app with user authentication, completely responsive and mobile friendly.",
+    img: "./disney.png",
+    iconLists: ["./re.svg", "./tail.svg", "./redux.png", "./fire.png"],
+    link: "https://disneyyhotstar.netlify.app",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Jaskaran Singh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Jaskaran Singh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Jaskaran Singh is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Jaskaran Singh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Jaskaran Singh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Jaskaran Singh is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Jaskaran was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Jaskaran Singh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Jaskaran Singh is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Jaskaran Singh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Jaskaran Singh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Jaskaran Singh is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Jaskaran Singh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Jaskaran Singh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Jaskaran Singh is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "./cloud.svg",
+    nameImg: "./cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "./app.svg",
+    nameImg: "./appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "./host.svg",
+    nameImg: "./hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "./s.svg",
+    nameImg: "./streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "./dock.svg",
+    nameImg: "./dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "SDE-1 Intern at Byteridge.",
+    desc: "Assisted as Full Stack Developer in the development of a web-based platform using Next.js, ASP.NET and enhancing interactivity.",
+    duration: "Jan 2024 - Mar 2024",
+    className: "md:col-span-2",
+    thumbnail: "./exp1.svg",
+  },
+  {
+    id: 2,
+    title: "AR Developer Intern at IELTS Proficiency.",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    duration: "Mar 2023 - Apr 2023",
+    className: "md:col-span-2",
+    thumbnail: "./exp1.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "./git.svg",
+    link: "https://github.com/JaskaranSingh20",
+  },
+  {
+    id: 2,
+    img: "./link.svg",
+    link: "https://www.linkedin.com/in/jaskaran-singh-83825a247/",
+  },
+];
